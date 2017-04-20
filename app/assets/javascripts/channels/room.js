8efa2b0667dd9ad53018e92a42d178f1b6abc1c2
Jeff Hostetler <hostetjm@gmail.com>
2017-04-19 22:59:42 -0600

    Add some basic styling and jquery so that most recent message is at the bottom of the chat box

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,12 +1,14 @@
-App.room = App.cable.subscriptions.create("RoomChannel", {
+App.room = App.cable.subscriptions.create('RoomChannel', {
   connected: function () {
-    //This don't do anything, but I am leaving it here so I don't forget to investigate
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
   },
   disconnected: function () {
     //This don't do anything, but I am leaving it here so I don't forget to investigate
   },
   received: function (data) {
-    $('#messages').append(data['message']);
+    $('#messages')
+      .append(data['message'])
+      .scrollTop($('#messages')[0].scrollHeight);
   },
   speak: function (text) {
     return this.perform('speak', {
